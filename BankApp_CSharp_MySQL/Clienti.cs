107b0740848c9a2ac79cd7cd93097b7fd5b1e3c9
Simina Rosioru <simina.rosioru@gmail.com>
2023-12-29 13:25:43 +0200

    Update Clienti.cs
    I added the information for the mysql connection, the no-parameter constructor and the one-parameter constructor.

--- BankApp_CSharp_MySQL/Clienti.cs
+++ BankApp_CSharp_MySQL/Clienti.cs
@@ -12,9 +12,21 @@ namespace BankApp_CSharp_MySQL
 {
     public partial class Clienti : Form
     {
+        //conexiunea cu sql
+        string conString= "server=localhost;uid=root;pwd=**********;database=bankapp_mysql_db";
+
+        //constructor fara parametrii
+        public Clienti()
+        {
+            InitializeComponent();
+        }
+
+        //constructor cu un parametru
         public Clienti(string idLogin)
         {
             InitializeComponent();
+            label6.Text = idLogin; //label6 nu se vede la compilare
+            label6.Visible = false;
         }
     }
 }
